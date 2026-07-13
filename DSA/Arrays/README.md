d4a02e4c86ea9989a057cc87e1aa30df0a9bf768
Anshika <anshika.g1007@gmail.com>
2026-07-13 12:48:31 +0530

    Add 'Maximum Sum Subarray of Size k' solution and 'Git Basics' notes.

---
+++ DSA/Arrays/README.md
@@ -0,0 +1,12 @@
+# Arrays
+
+## Topics
+|   Topic   |   Question Practiced  |   Revised    |
+|   Two Pointers  |   0    |   0   |
+|   Sliding Window    |   1    |   0    |
+|   Hashing   |   1    |   0    |
+
+
+## Problems Covered
+- 001. Two Sum
+- Maximum Sum Subarray of Size k  
--- DSA/Arrays/two_sum.py
+++ DSA/Arrays/two_sum.py
@@ -1,12 +1,28 @@
-# Two Sum
-
 '''
-Question:1. Two Sum
+Problem:1. Two Sum
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 '''
 
+#Approach:
+'''
+1. Create an empty dictionary (seen) to store numbers and their indices.
+2. Traverse the array once.
+3. For each number, calculate the required complement:
+    required = target - current_number
+4. If the complement already exists in the dictionary,return the current index and the stored index.
+5. Otherwise, store the current number with its index in the dictionary.
+6. Since dictionary lookup takes O(1) on average, the solution finds the answer in a single traversal.
+
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
+
+
+
+#Code
 def two_sum(nums, target):
     '''
     Return indices of two numbers whose sum is equal to target
