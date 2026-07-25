eba87b26ebd94ab04d4b8c186c657284ce9576a7
Anshika <anshika.g1007@gmail.com>
2026-07-25 21:28:07 +0530

    Add 'Fizz Buzz' solution

---
+++ DSA/String/README.md
@@ -0,0 +1,9 @@
+# Strings
+
+## Topics
+|   Topic   |   Question Practiced  |   Revised    |
+|   Basic  |   1   |   0   |
+
+
+## Problems Covered
+- 001. Fizz Buzz
---
+++ DSA/String/fizz_buzz.py
@@ -0,0 +1,25 @@
+'''
+Problem : Given an integer n, return a string array answer (1-indexed) where:
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+'''
+
+#Code
+def fizzBuzz(n):
+    answer=[]
+    for i in range(1,n+1):
+        if i%15==0 :
+            answer.append("FizzBuzz")
+        elif i%3==0:
+            answer.append("Fizz") 
+        elif i%5==0:
+            answer.append("Buzz")
+        else:
+                answer.append(str(i))
+    return answer
+
+n=int(input("Enter Array size:"))
+print(fizzBuzz(n))
