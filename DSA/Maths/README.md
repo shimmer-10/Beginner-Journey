68e0ee29413a5c91694008d2934c1a2a9ae23b1f
Anshika <anshika.g1007@gmail.com>
2026-08-16 07:49:28 +0530

    Add 'Palindrome Number solution'

---
+++ DSA/Maths/README.md
@@ -0,0 +1,4 @@
+# DSA based on Maths 
+
+## Problems Covered
+- 9. Palindrome Number
---
+++ DSA/Maths/palindrome_number.py
@@ -0,0 +1,15 @@
+'''
+Problem : Given an integer x, return true if x is a palindrome, and false otherwise.
+'''
+#Code
+def isPalindrome(x):
+    if x < 0 or (x!=0 and x%10==0):
+        return False
+    rev = 0
+    while x > rev :
+        rev = rev*10 + x%10
+        x//=10
+    return x == rev or x == rev//10
+
+num = int(input("Enter number :"))
+print(isPalindrome(num))
